71048568b87ae904a952e83894ad40a212ac7394
Andrew Hilts <andrewchilts@gmail.com>
2014-12-24 19:01:22 -0500

    make cookie identifer interface pretty level += 100

--- popup.js
+++ popup.js
@@ -31,6 +31,18 @@ window.addEventListener('message', function(event) {
   if (event.data.html) {
   console.log(event);
     report.innerHTML = event.data.html;
+    window.setTimeout(function(){
+      list = document.getElementById("badHosts");
+      highlightIdentifiers(list, 0, 0);
+      for(i in list.children){
+        list.children[i].onmouseover = function(){
+          window.clearTimeout(window.timer);
+          for(j in this.parentNode.children){
+            removeClass(this.parentNode.children[j], 'active');
+          }
+        }
+      }
+    }, 10);
   }
   // document.getElementsByTagName('body')[0].style.minHeight = report.offsetHeight + 200;
 });
@@ -44,23 +56,39 @@ chrome.runtime.sendMessage({
 });
 });
 
-window.onload = function(){
-  var reportEl = document.getElementById('report');
-  var faqEl = document.getElementById('faq');
-  var toggleEl = document.getElementById('about');
-  var toggleState = 1;
-  toggleEl.onclick = function(){
-    if(toggleState === 1){
-      reportEl.style.display = "none";
-      faqEl.style.display = "block";
-      toggleState = 2;
+highlightIdentifiers = function(e, oldIndex, newIndex){
+  console.log(oldIndex, newIndex);
+  removeClass(e.children[oldIndex], "active");
+  addClass(e.children[newIndex], "active");
+  window.timer = window.setTimeout(function(){
+    console.log(e.children.length)
+    if(e.children.length == newIndex+1){
+      newerIndex = 0;
     }
     else{
-      reportEl.style.display = "block";
-      faqEl.style.display = "none";
-      toggleState = 1;
+      var newerIndex = newIndex+1;
     }
-  }
+    highlightIdentifiers(e, newIndex, newerIndex);
+  }, 1000);
+}
+
+window.onload = function(){
+  var reportEl = document.getElementById('report');
+  // var faqEl = document.getElementById('faq');
+  // var toggleEl = document.getElementById('about');
+  // var toggleState = 1;
+  // toggleEl.onclick = function(){
+  //   if(toggleState === 1){
+  //     reportEl.style.display = "none";
+  //     faqEl.style.display = "block";
+  //     toggleState = 2;
+  //   }
+  //   else{
+  //     reportEl.style.display = "block";
+  //     faqEl.style.display = "none";
+  //     toggleState = 1;
+  //   }
+  // }
 }
 
 var populateTrackerLists = function(tabData){
@@ -69,4 +97,19 @@ var populateTrackerLists = function(tabData){
    context: tabData
   };
   iframe.contentWindow.postMessage(message, '*');
+}
+
+function hasClass(ele,cls) {
+  return ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
+}
+
+function addClass(ele,cls) {
+  if (!hasClass(ele,cls)) ele.className += " "+cls;
+}
+
+function removeClass(ele,cls) {
+  if (hasClass(ele,cls)) {
+    var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
+    ele.className=ele.className.replace(reg,' ');
+  }
 }
