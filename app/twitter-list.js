0fea4d56be73dabc4d8b999ea7249e0e183d45ac
Andrew Hilts <andrewchilts@gmail.com>
2015-01-14 18:58:12 -0500

    make language more unified: remove SSL ref, replace with HTTPS

--- app/twitter-list.js
+++ app/twitter-list.js
@@ -3,12 +3,7 @@ var TwitterList = {
 	listFile: "twitterLists/defaultList.json",
 	getList: function(){
 		var xhr = new XMLHttpRequest();
-	    // Use blocking XHR to ensure everything is loaded by the time
-	    // we return.
-	    //var that = this;
-	    //xhr.onreadystatechange = function() { that.loadRuleSet(xhr); }
 	    xhr.open("GET", chrome.extension.getURL(this.listFile), false);
-	    //xhr.open("GET", chrome.extension.getURL(rule_list[i]), true);
 	    xhr.send(null);
 	    return this.loadList(xhr.response);
 	},
